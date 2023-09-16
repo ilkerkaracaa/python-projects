f1f2ff1ce7621b29ad45abfb69da9b5c44cd543c
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-16 12:20:43 +0300

    fireee

--- Alien Invasion/ship.py
+++ Alien Invasion/ship.py
@@ -9,14 +9,18 @@ class Ship:
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
+
         # Load the ship image and get its rect.
-        self.image = pygame.image.load("images/ship.bmp")
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
         # Store a float for the ship's exact horizontal position.
         self.x = float(self.rect.x)
-        # Movement flag; start with a ship that's not moving.
+
+        # Movement flags; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
 
@@ -27,6 +31,7 @@ class Ship:
             self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
+            
         # Update rect object from self.x.
         self.rect.x = self.x
 
