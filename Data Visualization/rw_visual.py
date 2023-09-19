a31acc0cf618ecd8f3b44a3c558d25547d081edb
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-19 22:12:54 +0300

    data Visualization random data

--- Data Visualization/rw_visual.py
+++ Data Visualization/rw_visual.py
@@ -3,12 +3,14 @@ from random_walk import RandomWalk
 
 while True:
     # Make a random walk.
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk.
     plt.style.use("classic")
-    fig, ax = plt.subplots()
+    # fig, ax = plt.subplots()
+    # fig, ax = plt.subplots(figsize=(15, 9))
+    fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
     point_numbers = range(rw.num_points)
     ax.scatter(
         rw.x_values,
@@ -16,13 +18,16 @@ while True:
         c=point_numbers,
         cmap=plt.cm.Blues,
         edgecolors="none",
-        s=15,
+        s=1,
     )
     # ax.scatter(rw.x_values, rw.y_values, s=15)
     ax.set_aspect("equal")
     # Emphasize the first and last points.
     ax.scatter(0, 0, c="green", edgecolors="none", s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c="red", edgecolors="none", s=100)
+    # Remove the axes.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plt.show()
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == "n":
