08a2d9eb56fa2c537295768e944effc56af2d24c
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-19 22:24:37 +0300

    updated

--- Data Visualization/rw_visual.py
+++ Data Visualization/rw_visual.py
@@ -3,7 +3,7 @@ from random_walk import RandomWalk
 
 while True:
     # Make a random walk.
-    rw = RandomWalk(50_000)
+    rw = RandomWalk(50_00)
     rw.fill_walk()
 
     # Plot the points in the walk.
@@ -12,14 +12,15 @@ while True:
     # fig, ax = plt.subplots(figsize=(15, 9))
     fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
     point_numbers = range(rw.num_points)
-    ax.scatter(
-        rw.x_values,
-        rw.y_values,
-        c=point_numbers,
-        cmap=plt.cm.Blues,
-        edgecolors="none",
-        s=1,
-    )
+    # ax.scatter(
+    #     rw.x_values,
+    #     rw.y_values,
+    #     c=point_numbers,
+    #     cmap=plt.cm.Blues,
+    #     edgecolors="none",
+    #     s=1,
+    # )
+    ax.plot(rw.x_values, rw.y_values, linewidth=1)
     # ax.scatter(rw.x_values, rw.y_values, s=15)
     ax.set_aspect("equal")
     # Emphasize the first and last points.
