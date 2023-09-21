cb3715730408d8e07780573717f555f04c573153
İlker Karaca <ikaracadeveloper@gmail.com>
2023-09-21 21:02:00 +0300

    updated

--- Data Visualization/sitka_highs_lows.py
+++ Data Visualization/sitka_highs_lows.py
@@ -4,32 +4,35 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 
 
-path = Path("weather_data/sitka_weather_07-2021_simple.csv")
+path = Path("weather_data/sitka_weather_2021_simple.csv")
 lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
-print(header_row)
+# print(header_row)
 # print(lines)
 
 for index, column_header in enumerate(header_row):
     print(index, column_header)
 
 # Extract high temperatures.
-dates, highs = [], []
+dates, highs, lows = [], [], []
 for row in reader:
     current_date = datetime.strptime(row[2], "%Y-%m-%d")
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
-print(highs)
+    lows.append(low)
+# print(highs)
 
 # Plot the high temperatures.
 plt.style.use("seaborn")
 fig, ax = plt.subplots()
 # ax.plot(highs, color="red")
 ax.plot(dates, highs, color="red")
+ax.plot(dates, lows, color="blue")
 # Format plot.
-ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_title("Daily High and Low Temperatures, 2021", fontsize=24)
 ax.set_xlabel("Days", fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
